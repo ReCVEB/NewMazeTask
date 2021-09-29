86fdeaf5fa3f560ec380817bece83968828742a8
CarolHe <carol.hcxy@gmail.com>
2021-09-29 11:00:05 -0700

    mazewall-intermission-testingPhaseVisibility-update

--- Assets/Scripts/MazeUtility/TimeManager.cs
+++ Assets/Scripts/MazeUtility/TimeManager.cs
@@ -23,7 +23,7 @@ public class TimeManager : MonoBehaviour {
     private string objectName = "";
     MazeManager mazeManager;
     TrackPlayer logManager;
-    //IntermissionManager intermissionManager;
+    IntermissionManager intermissionManager;
 
     private void Awake()
     {
@@ -33,7 +33,7 @@ public class TimeManager : MonoBehaviour {
 		timeCount = TimeLimit;
 		mazeManager = MazeManager.instance;
 		logManager = TrackPlayer.instance;
-        //intermissionManager = IntermissionManager.instance;
+        intermissionManager = IntermissionManager.instance;
 	}
 	void Update () {
 		if (shouldCount){
@@ -81,8 +81,8 @@ public class TimeManager : MonoBehaviour {
 					Reset();
 					mazeManager.ChooseLevel();
                     mazeManager.UnloadLevel();
-                    //intermissionManager.ActivatePoints();
-                    mazeManager.PrepareLevel();
+                    intermissionManager.ActivatePoints();
+                    //mazeManager.PrepareLevel();
 				}
 			}
 		}
@@ -93,8 +93,8 @@ public class TimeManager : MonoBehaviour {
 			Reset();
 			mazeManager.ChooseLevel();
 			mazeManager.UnloadLevel();
-            mazeManager.PrepareLevel();
-            //intermissionManager.ActivatePoints();
+            //mazeManager.PrepareLevel();
+            intermissionManager.ActivatePoints();
         }
 	}
 	public void Reset(){
